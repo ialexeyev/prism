f1fb47403bd8ff028e7f889e60785e622a058652
ialexeyev <98263107+ialexeyev@users.noreply.github.com>
2025-06-30 08:37:23 +0000

    8. Adaptive design was setted.

--- static/asset/general.js
+++ static/asset/general.js
@@ -7,7 +7,28 @@ function displayUserImg(img) {
       img.style.scale = "1";
     }
     else {
-      img.style.translate = "-3vw 3vw";
+      // for all screen sizes
+      if (window.innerWidth >= 1824) {
+        img.style.translate = "-2.8vw 2.8vw"; // Large Screens
+      }
+      if ((window.innerWidth < 1824) && (window.innerWidth >= 1224)) {
+        img.style.translate = "-3vw 3vw"; // Desktop PC
+      }
+      if((window.innerWidth < 1224) && (window.innerWidth >= 1024)) {
+        img.style.translate = "-4vw 4vw"; // Laptop
+      }
+      if((window.innerWidth < 1024) && (window.innerWidth >= 768)) {
+        img.style.translate = "-4.5vw 4.5vw"; // Net Laptop
+      }
+      if((window.innerWidth < 768) && (window.innerWidth >= 480)) {
+        img.style.translate = "-6.9vw 6.9vw"; // Pad
+      }
+      if((window.innerWidth < 480) && (window.innerWidth >= 360)) {
+        img.style.translate = "-9vw 9vw"; // Smartphone
+      }
+      if(window.innerWidth < 360) {
+        img.style.translate = "-9.2vw 9.2vw"; // Mobile
+      }
       img.style.scale = "5";
     }
 }
