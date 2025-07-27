931251f16c94390ed45ea8f4c21c7b0a3d316248
ialexeyev <98263107+ialexeyev@users.noreply.github.com>
2025-07-27 08:51:16 +0000

    13. sign up form added with css

--- static/asset/home.js
+++ static/asset/home.js
@@ -6,11 +6,25 @@ window.onload = () => {
 
 function appstart(obj) {
   obj.style.opacity = "0";
+  document.getElementById('signinimg').style.display = "block";
   setTimeout(() => {
     obj.style.display = "none";
     document.getElementById('signinimg').style.transform = "rotateY(0deg)";
   }, "1000");
-  setTimeout(() => {
+  setTimeout(() => {  
     document.getElementById('signInFormBlock').style.transform = "rotateX(0deg)";
   }, "2000");
+}
+
+
+function openSignUp(obj) {
+  document.getElementById('signInFormBlock').style.transform = "rotateX(90deg)";
+  document.getElementById('signUpFormBlock').style.display = "block";
+  setTimeout(() => {
+    document.getElementById('signUpFormBlock').style.transform = "rotateX(0deg)";
+  }, "1000");
+}
+
+function selectChangeColor(obj) {
+  obj.style.color = "black";
 }
