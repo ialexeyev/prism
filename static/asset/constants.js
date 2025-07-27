35c25dd182b54fa749fd3c35403183547ae712b6
ialexeyev <98263107+ialexeyev@users.noreply.github.com>
2025-07-27 11:03:16 +0000

    15. Home page js started

---
+++ static/asset/constants.js
@@ -0,0 +1,4 @@
+/* SKILL UP application constants */
+
+/* 1. Sign in and sign up errors */
+const ERR_MSG_TXT_ID_EMPTY = 'ENTER YOUR ID!';
--- static/asset/home.js
+++ static/asset/home.js
@@ -4,6 +4,7 @@ window.onload = () => {
   document.body.style.opacity = "1";
 }
 
+/* Starting application */
 function appstart(obj) {
   obj.style.opacity = "0";
   document.getElementById('signinimg').style.display = "block";
@@ -16,7 +17,7 @@ function appstart(obj) {
   }, "2000");
 }
 
-
+/* Opening registration form */
 function openSignUp(obj) {
   document.getElementById('signInFormBlock').style.transform = "rotateX(90deg)";
   document.getElementById('signUpFormBlock').style.display = "block";
@@ -25,6 +26,48 @@ function openSignUp(obj) {
   }, "1000");
 }
 
+/* changing color on select field inside registration form */
 function selectChangeColor(obj) {
   obj.style.color = "black";
+}
+
+
+/* Sign in main function */
+function signIn(id, passw, err) {
+  /* 1. check grammar mistakes */
+  checkSignIn(id, passw, err);
+  
+}
+
+/* Checking sign in form (frontend) */
+function checkSignIn(userId, userPassw, errObj) {
+  if(userId.value == "") {
+    showErrorMsg(errObj, ERR_MSG_TXT_ID_EMPTY);
+    paintErrField(userId);
+    return false;
+  }
+}
+
+/* Show error message */
+function showErrorMsg(errorObj, msg) {
+  errorObj.style.color = "red";
+  errorObj.textContent = msg;
+  errorObj.style.visibility = "visible";
+}
+
+/* Clear error message & fields */
+function clearErrors(obj, err) {
+  obj.style.background = "white";
+  err.style.visibility = "hidden";
+  clearErrField(obj);
+}
+
+/* Paint error field */
+function paintErrField(obj) {
+  obj.style.background = "#ecdada";
+}
+
+/* Clear error field */ 
+function clearErrField(obj) {
+  obj.style.background = "white";
 }
